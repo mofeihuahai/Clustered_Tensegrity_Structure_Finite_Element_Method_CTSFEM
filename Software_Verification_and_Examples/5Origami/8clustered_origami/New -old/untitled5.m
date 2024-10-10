72b787c10bb612afeb4e44e4cb1217d1a259f87d
mashuo10 <mashuo@zju.edu.cn>
2024-10-10 17:05:05 +0800

    Update untitled5.m

--- Software_Verification_and_Examples/5Origami/8clustered_origami/New -old/untitled5.m
+++ Software_Verification_and_Examples/5Origami/8clustered_origami/New -old/untitled5.m
@@ -1,6 +1,6 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%A Clustered Cable Net(deployable)%%%%%%%%
+%%%%%%membrane element%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % /* This Source Code Form is subject to the terms of the Mozilla Public
 % * License, v. 2.0. If a copy of the MPL was not distributed with this
